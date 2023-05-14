8c5d5307912118effd8cb2064ceb6e49ca240d13
mihirs16 <mihirs16@gmail.com>
2023-05-14 18:54:44 +0100

    feat: stats for ants | fix: blocking message buffer and synchronous message queue

--- Queen.cs
+++ Queen.cs
@@ -7,13 +7,15 @@ public class Queen {
     public readonly int numMessages;
     public readonly CancellationTokenSource cancellationTokenSource;
     protected Dictionary<int, Ant> ants;
-    private BlockingCollection<Message> messages;
+    private BlockingCollection<Message> buffer;
+    private Queue<Message> messages;
 
     public Queen (int numAnts, int numMessages) {
         this.numAnts = numAnts;
         this.numMessages = numMessages;
         this.ants = new Dictionary<int, Ant>();
-        this.messages = new BlockingCollection<Message>(this.numMessages);
+        this.buffer = new BlockingCollection<Message>(this.numMessages);
+        this.messages = new Queue<Message>(this.numMessages);
         this.cancellationTokenSource = new CancellationTokenSource();
 
         Console.WriteLine($"Spawning {numAnts} ants...");
@@ -22,19 +24,48 @@ public class Queen {
             this.ants[i] = new Ant(this, faker.Person.UserName);
         }
 
+        List<Thread> threads = new List<Thread>();
         Console.WriteLine("Starting ants' behaviour...");
-        for (int i = 0; i < numAnts; i++)
-            new Thread(this.ants[i].Behaviour).Start();
+        for (int i = 0; i < numAnts; i++) {
+            Thread thread = new Thread(this.ants[i].Behaviour);
+            threads.Add(thread);
+            thread.Start();
+        }
 
-        for (int i = 0; i < numMessages; i++)
-            Console.WriteLine(messages.Take());
+        for (int i = 0; i < numMessages; i++) {
+            Message eachMessage = buffer.Take();
+            Console.WriteLine($"{eachMessage}");
+            messages.Enqueue(eachMessage);
+        }
         
         this.cancellationTokenSource.Cancel();
+        for (int i = 0; i < numAnts; i++) {
+            threads[i].Join();
+        }
         return;
     }
 
     public Ant getAntFromId (int id) => this.ants[id];
-    public void sendMessage (Message message) => this.messages.Add(message);
+    public void sendMessage (Message message) => this.buffer.Add(message);
+    
+    public void displayStats () {
+        Console.WriteLine($"Number of ants: {this.numAnts}");
+        Console.WriteLine($"Number of messages: {this.messages.Count}");
+
+        IEnumerable<IGrouping<string, Message>> messagesGroupedBySender = 
+            from message in this.messages
+            group message by message.sender.name into groupedBySender
+            select groupedBySender;
+        foreach (var messageGroup in messagesGroupedBySender)
+            Console.WriteLine($"{messageGroup.First().sender} sent {messageGroup.Count()} messages.");
+
+        IEnumerable<IGrouping<string, Message>> messagesGroupedByReceiver =
+            from message in this.messages
+            group message by message.receiver.name into groupedByReceiver
+            select groupedByReceiver;
+        foreach (var messageGroup in messagesGroupedByReceiver)
+            Console.WriteLine($"{messageGroup.First().receiver} received {messageGroup.Count()} messages.");
+    }
 }
 
 class Program {
@@ -52,5 +83,8 @@ class Program {
         
         Console.WriteLine("Spawning a queen...");
         Queen queen = new Queen(numAnts, numMessages);
+
+        Console.WriteLine("Displaying stats...");
+        queen.displayStats();
     }
 }
